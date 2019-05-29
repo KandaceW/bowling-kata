0bb964c6e2ada5f4399cc97002a4f06a10af5fae
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2019-05-29 17:22:23 +1200

    add function spare

--- game.js
+++ game.js
@@ -1,7 +1,16 @@
 // Score 119:
-// var frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
-// ]
+var frames = [
+  [1, 2],
+  [6, 4],
+  [5, 4],
+  [10, 0],
+  [7, 2],
+  [10, 0],
+  [10, 0],
+  [5, 2],
+  [7, 0],
+  [4, 4]
+];
 //
 // Score 141:
 // var frames = [
@@ -12,3 +21,27 @@
 // var frames = [
 //  [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10],
 // ]
+
+// function totalScore(frames) {
+//   let newArr = [];
+//   for (let i = 0; i < frames.length; i++) {
+//     // if frames[i] frames[i][0] to frames[i][1]
+//       frames[i] = (frames[i][0] + frames[i][1]);
+//       newArr.push(frames[i]);
+//   }
+//   return newArr
+// }
+
+// console.log(totalScore(frames));
+
+function spare(frames) {
+  let something = frames;
+  for (let i = 0; i < frames.length; i++) {
+    if (frames[i][0] + frames[i][1] === 10 && frames[i][1] !== 0) {
+     frames[i + 1] + frames[i][0];
+    }
+  }
+  return something;
+}
+
+console.log(spare(frames));

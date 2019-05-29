23b98cb230cbd1da1da92a88c48b966544c96c3a
Pano Skylakis <pano_skylakis@outlook.com>
2019-05-29 23:57:49 +1200

    add solution for all except final arr

--- game.js
+++ game.js
@@ -1,47 +1,96 @@
 // Score 119:
-var frames = [
-  [1, 2],
-  [6, 4],
-  [5, 4],
-  [10, 0],
-  [7, 2],
-  [10, 0],
-  [10, 0],
-  [5, 2],
-  [7, 0],
-  [4, 4]
-];
+// var frames = [[1, 2],  [6, 4],  [5, 4],  [10, 0],  [7, 2], [10, 0],  [10, 0],  [5, 2],  [7, 0], [4, 4]
+// ];
 //
 // Score 141:
 // var frames = [
 //   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
 // ]
 //
-//Score 300:
-// var frames = [
-//  [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10],
-// ]
+// Score 300:
+var frames = [
+ [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10],
+]
 
-// function totalScore(frames) {
-//   let newArr = [];
-//   for (let i = 0; i < frames.length; i++) {
-//     // if frames[i] frames[i][0] to frames[i][1]
-//       frames[i] = (frames[i][0] + frames[i][1]);
-//       newArr.push(frames[i]);
-//   }
-//   return newArr
-// }
 
-// console.log(totalScore(frames));
-
-function spare(frames) {
-  let something = frames;
-  for (let i = 0; i < frames.length; i++) {
-    if (frames[i][0] + frames[i][1] === 10 && frames[i][1] !== 0) {
-     frames[i + 1] + frames[i][0];
-    }
-  }
-  return something;
+function finalScore(frames) {
+  checkSpare(frames);
+  checkThreeStrikes(frames);
+  // checkLastThreeStrikes(frames);
+  checkStrike(frames);
+  frameScore(frames);
+  return total(frames)
 }
 
-console.log(spare(frames));
+
+
+function frameScore(frames) {
+  for (let i = 0; i < frames.length; i++) {
+    frames[i] = (frames[i][0] + frames[i][1]);
+  }
+}
+
+
+
+function checkSpare(frames) {
+  for (let i = 0; i < frames.length; i++) {
+    if (frames[i][0] + frames[i][1] === 10 && frames[i][1] !== 0) {
+      frames[i] = (frames[i][0] + frames[i][1] + frames[i+1][0]) + [];
+    }
+  }
+  return frames
+}
+
+
+function checkThreeStrikes(frames) {
+  for (let i = 0; i < frames.length; i++) {
+    if (frames[i][0] === 10 && frames[i+1] && frames[i+1][0] === 10 && frames[i+2] && frames[i+2][0] === 10) {
+      frames[i] = (frames[i][0] + frames[i+1][0] + frames[i+2][0]) + [];
+    }
+  }
+  return frames;
+}
+
+
+
+function checkLastThreeStrikes(frames) {
+  for (let i = 0; i < frames.length; i++) {
+    if (frames[i][0] === 10 && frames[i][1] === 10 && frames[i][2] === 10) {
+      frames[i] = 30 + [];
+    }
+  }
+  return frames
+}
+
+
+
+function checkStrike(frames) {
+  for (let i = 0; i < frames.length; i++) {
+    if (frames[i][0] === 10 && frames[i+1][0] === 10) {
+      frames[i] = (frames[i][0] + frames[i+1][0] + frames[i+2][0]) + [];
+    } else if (frames[i][0] === 10 && frames[+1][0] !== 10) {
+      frames[i] = (frames[i][0] + frames[i][1]) + (frames[i+1][0] + frames[i+1][1]) + [];
+    }
+  }
+  return frames
+}
+
+
+
+function total(frames) {
+  frames.join(' ').split('');
+  return frames.map(Number).reduce((a, b) => a + b, 0);
+}
+
+
+
+// console.log(finalScore(frames));
+
+// console.log(checkSpare(frames));
+// console.log(checkThreeStrikes(frames));
+// console.log(checkLastThreeStrikes(frames));
+console.log(checkStrike(frames));
+// console.log(frameScore(frames))
+// console.log(total(frames));
+
+

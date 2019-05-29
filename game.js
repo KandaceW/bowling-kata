3b2663af06928e5dbda2d06026469ce5e3ccca7b
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-05-29 19:09:46 +1200

    Add more workings to game.js

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 // Score 119:
-var frames = [
-  [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
-]
+// var frames = [
+//   [1, 1], [1, 1], [1, 1], [10, 0], [1, 1], [2, 2], [1, 1], [1, 1], [7,1], [1, 1]
+// ]
 //
 // Score 141:
-// var frames = [
-//   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
-// ]
+var frames = [
+  [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
+]
 //
 //Score 300:
 // var frames = [
@@ -24,17 +24,31 @@ function score(game) {
   for (let i = 0; i < game.length; i++) {
 
     let frame = game[i]
+    let secondFrame = game[i + 1]
+    let thirdFrame = game[i + 2]
 
-    let scoreOfFrame = addScore(frame)
+    console.log('Hi I am the first frame: ' , frame)
+    console.log('Hi I am that frame\'s buddy: ' , secondFrame)
+    console.log('Hi I am that frame\'s second buddy: ' , thirdFrame)
 
-    result += scoreOfFrame
+    if(checkStrike(frame)){
+
+      result += addStrikeScore(frame, secondFrame, thirdFrame)
+
+    } else if (checkSpare(frame)){
+
+      result += addSpareScore(frame, secondFrame)
+
+    } else {
+
+      result += addScore(frame)
+    
+    }
 
   }
 
   console.log('The final result is' , result)
   return result
-
-  
 }
 
 
@@ -42,7 +56,23 @@ function addScore(frame) {
   return frame[0] + frame[1]
 }
 
-function spare(frame) {
+function addStrikeScore(frame, secondFrame, thirdFrame) {
+
+  if(checkDoubleStrike(frame, secondFrame)){
+    return frame[0] + frame[1] + secondFrame[0] + secondFrame[1] + thirdFrame[0] + thirdFrame[1]
+  }
+
+  return frame[0] + frame[1] + secondFrame[0] + secondFrame[1]
+
+}
+
+function addSpareScore(frame, secondFrame) {
+  return frame[0] + frame[1] + secondFrame[0]
+}
+
+
+// Check if there is a spare - working!
+function checkSpare(frame) {
   if(addScore(frame) === 10 && frame[0] !== 10){
     return true
   } else {
@@ -50,7 +80,8 @@ function spare(frame) {
   }
 }
 
-function strike(frame) {
+// Check if there is a strike - working!
+function checkStrike(frame) {
   if(frame[0] === 10){
     return true
   } else {
@@ -58,7 +89,16 @@ function strike(frame) {
   }
 }
 
+// Check if there is a double strike
+function checkDoubleStrike(frame, secondFrame){
+  if(frame[0] === 10 && secondFrame[0] === 10){
+    return true
+  } else {
+    return false
+  }
+}
 
+// Call the function
 score(frames);
 
 
